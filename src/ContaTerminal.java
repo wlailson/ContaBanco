4943421a8c8e930fd97d57f95cd3ed32e4e77e1d
wlailson <vlalves4124@gmail.com>
2024-08-27 09:24:34 -0300

    dio-trilha

---
+++ src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        double saldo = 237.48;
+        //recebendo parametros
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da Agência");
+        String agencia =  scanner.next() ;
+
+        System.out.println("Por favor, digite o seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta");
+        int numero = scanner.nextInt();
+        
+        // imprimindo os parametros
+        System.out.print("Olá " + nome + ", Obrigado por criar uma conta em nosso banco,");
+        System.out.print(" sua agência é " + agencia);
+        System.out.print(", conta " + numero);
+        System.out.print(" e seu saldo " + saldo + " ja esta disponivel para saque");
+
+
+    }
+}
